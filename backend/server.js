664b06fe7420ac786e3967109564133a20019245
hariari0617-jpg <hariari0617@gmail.com>
2026-09-26 00:10:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -308,7 +308,7 @@ app.use(
 // START SERVER
 // ===============================
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
     console.log(
         `Server running on port ${PORT}`
